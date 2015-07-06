ad59296f740f141faa35a6517b536f3a20fde783
garethfoote <gareth.foote@gmail.com>
2015-07-06 15:08:21 +0100

    Adding advanced settings checkboxgs

--- custom-fields/BaseWidget.php
+++ custom-fields/BaseWidget.php
@@ -1,7 +1,28 @@
 <?php
 class BaseWidget {
   public static function addWidgetDeviceTargetting($widgetConfig) {
-    $displayOnDevices = array (
+
+    $widgetConfig["sub_fields"][] = array (
+      'key' => $widgetConfig["key"] . '_show_settings',
+      'label' => 'Show advanced settings',
+      'name' => 'show_advanced_settings',
+      'type' => 'true_false',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => 'advanced-widget-settings',
+        'id' => '',
+      ),
+      'choices' => array (
+        '' => '',
+      ),
+      'default_value' => 0,
+      'layout' => 'vertical',
+    );
+
+    $widgetConfig["sub_fields"][] = array (
       'key' => $widgetConfig["key"] . '_device_targetting',
       'label' => 'Display on devices',
       'name' => 'display_on_devices',
@@ -9,7 +30,15 @@ class BaseWidget {
       'type' => 'checkbox',
       'instructions' => '',
       'required' => 0,
-      'conditional_logic' => 0,
+      'conditional_logic' => array (
+        array (
+          array (
+            'field' => $widgetConfig["key"] . '_show_settings',
+            'operator' => '==',
+            'value' => '1',
+          ),
+        ),
+      ),
       'wrapper' => array (
         'width' => '',
         'class' => 'display-on-devices',
@@ -28,7 +57,6 @@ class BaseWidget {
       'layout' => 'vertical',
     );
 
-    $widgetConfig["sub_fields"][] = $displayOnDevices;
     return $widgetConfig;
   }
 }
