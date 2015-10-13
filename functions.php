6dda06627b2f2f5f1130009c90a92c3939190a69
Simon Donaldson <simondonaldson@gmail.com>
2015-10-13 10:43:48 +0100

    Set Plain Text Paste to on by default

--- functions.php
+++ functions.php
@@ -223,6 +223,8 @@ HTML;
     $init['block_formats'] = "Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Preformatted=pre";
     //make sure kitchen sink is displayed by default
     $init['wordpress_adv_hidden'] = true;
+    //force 'plain text paste' to be on
+    $init['paste_as_text'] = true;
     return $init;
   }
 
