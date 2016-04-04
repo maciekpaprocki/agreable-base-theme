ed0e66895b39ce5170a96924f3cd7cb825b3f4df
ecoad <elliot.coad@shortlist.com>
2016-04-04 14:49:44 +0100

    Category parent retrieved if only child is present

--- libs/services/CategoryService.php
+++ libs/services/CategoryService.php
@@ -7,6 +7,15 @@ class AgreableCategoryService {
   public static function get_post_category_hierarchy($post) {
 
     $post_categories = \wp_get_post_categories($post->ID);
+
+    // If just the one category, but it has a parent, add it to the array
+    if(count($post_categories) === 1) {
+      $category = get_category($post_categories[0]);
+      if ($category && $category->parent) {
+        $post_categories[] = $category->parent;
+      }
+    };
+
     $hierarchy_categories = new stdClass();
     $hierarchy_categories->child = null;
     $hierarchy_categories->parent = null;
@@ -19,6 +28,7 @@ class AgreableCategoryService {
 
       if ($cat->parent) { // This is a child category
         $hierarchy_categories->child = $category;
+        $hierarchy_categories->parent = new stdClass();
       } else {
         $hierarchy_categories->parent = $category;
       }
