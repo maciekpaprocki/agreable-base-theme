a89545a5bf6b08f917e1dd45278d6adc1d1c9dbf
ecoad <elliotcoad@gmail.com>
2016-04-20 17:23:06 +0100

    Neatened up the admin JS

--- admin-customisations/agreable-admin.js
+++ admin-customisations/agreable-admin.js
@@ -1,12 +1,18 @@
 jQuery(function() {
-  console.log('Agreable - listening for options checkbox')
-  jQuery(document).on('click', '.agreable-options-controller input[type=checkbox]', function() {
-    var showOptions = $(this).is(':checked')
-    var $parent = $(this).parents('.acf-fields').eq(0)
-    if (showOptions) {
-      $parent.addClass('agreable-options-enabled')
-    } else {
-      $parent.removeClass('agreable-options-enabled')
-    }
-  })
+  console.log('Agreable - admin JS')
+
+  function listenForOptionsCheckbox() {
+    console.log('Agreable - listening for options checkbox')
+    jQuery(document).on('click', '.agreable-options-controller input[type=checkbox]', function() {
+      var showOptions = $(this).is(':checked')
+      var $parent = $(this).parents('.acf-fields').eq(0)
+      if (showOptions) {
+        $parent.addClass('agreable-options-enabled')
+      } else {
+        $parent.removeClass('agreable-options-enabled')
+      }
+    })
+  }
+
+  listenForOptionsCheckbox()
 })
