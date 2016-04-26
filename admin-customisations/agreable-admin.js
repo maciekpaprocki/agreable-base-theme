b95777c0d16340507de176b8838ea5544e387d73
Kimberley Cook <kimberleycook@Kimberleys-MacBook-Pro.local>
2016-04-26 14:08:18 +0100

    chanegd $ to jquery

--- admin-customisations/agreable-admin.js
+++ admin-customisations/agreable-admin.js
@@ -4,8 +4,8 @@ jQuery(function() {
   function listenForOptionsCheckbox() {
     console.log('Agreable - listening for options checkbox')
     jQuery(document).on('click', '.agreable-options-controller input[type=checkbox]', function() {
-      var showOptions = $(this).is(':checked')
-      var $parent = $(this).parents('.acf-fields').eq(0)
+      var showOptions = jQuery(this).is(':checked')
+      var $parent = jQuery(this).parents('.acf-fields').eq(0)
       if (showOptions) {
         $parent.addClass('agreable-options-enabled')
       } else {
