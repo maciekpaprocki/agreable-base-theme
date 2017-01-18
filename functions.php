f7d328744fc57f8f78856cfe4455201445853c45
dan_adeyemi <iamdeaneyelid@gmail.com>
2017-01-18 16:04:11 +0000

    making sure current user is there before running code

--- functions.php
+++ functions.php
@@ -254,10 +254,11 @@ class AgreableBase extends TimberSite {
     include_once('custom-fields/list/list.php');
     include_once('custom-fields/tile/tile.php');
     include_once("custom-fields/options/options-page.php");
-    $user = wp_get_current_user();
-    $user_roles = $user->roles;
-    if (in_array('administrator', $user_roles)) {
-      acf_add_options_page();
+    if ($user = wp_get_current_user()) {
+      $user_roles = $user->roles;
+      if (in_array('administrator', $user_roles)) {
+        acf_add_options_page();
+      }
     }
   }
 
